b3ebad41ed685858ab5f7628e6d4bd85e645d4e6
Navneet Kaur <nbirdi@yahoo.com>
2022-10-02 14:45:23 +0800

    some styles added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+    
+    <div className="container header_container">
+    <h5>Hello I'm</h5>
+    <h1>Navneet Kaur</h1>
+    <h5 className="text-align">Fullstack Developer</h5>
+    <CTA/>
+    <HeaderSocial />
+    <div className="me">
+    <img src={ME} alt="me"/>
+    </div>
+    
+    <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+    </div>
+   </header>
   )
 }
 
